97c97b16b7e5261c833cd4c98061832a003c31c8
Lucas <lucasgoc@al.insper.edu.br>
2026-08-11 12:02:22 -0300

    parte 4 ta acabando

--- servidor.py
+++ servidor.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template_string
+from flask import Flask, render_template_string, request, redirect
 import views
+
 
 app = Flask(__name__)
 
@@ -8,7 +9,16 @@ app.static_folder = 'static'
 
 @app.route('/')
 def index():
+
     return render_template_string(views.index())
+
+@app.route('/submit', methods=['POST'])
+def submit_form():
+    titulo = request.form.get('titulo')  # Obtém o valor do campo 'titulo'
+    detalhes = request.form.get('detalhes')  # Obtém o valor do campo 'detalhes'
+
+    views.submit(titulo, detalhes)
+    return redirect('/')
 
 if __name__ == '__main__':
     app.run(debug=True)
